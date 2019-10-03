d952374c546d3b07134f6e98a4b485aa36e32c40
Viddya Sagar <viddya.verma@toyotaconnected.co.in>
2019-10-03 18:23:31 +0530

    Added test cases.

--- test/crc_itu_test.exs
+++ test/crc_itu_test.exs
@@ -1,8 +1,13 @@
-defmodule CrcItuTest do
+defmodule CRCITUTest do
   use ExUnit.Case
-  doctest CrcItu
+  doctest CRCITU
 
-  test "greets the world" do
-    assert CrcItu.hello() == :world
+  test "Test1" do
+    assert CRCITU.crc_itu("05010001") == "D9DC"
   end
+
+  test "Test2" do
+    assert CRCITU.crc_itu("0D0101234567890123450001") == "8CDD"
+  end
+
 end
